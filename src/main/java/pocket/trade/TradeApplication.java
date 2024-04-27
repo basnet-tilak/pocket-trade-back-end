9e498d705aa03696c95885be7de92d102f82096f
Tilak Basnet <tilak.basnet@outlook.com>
2024-04-27 11:29:14 +0200

    Has been added the Customer and employee package and classes.

---
+++ src/main/java/pocket/trade/TradeApplication.java
@@ -0,0 +1,11 @@
+package pocket.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TradeApplication.class, args);
+    }
+}
